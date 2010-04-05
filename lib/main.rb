587633b2f7f38df950b511edcaa588d732cde43c
Fahad Sadah <fahadsadah@gmail.com>
2010-04-05 08:57:52 +0100

    Initial import

---
+++ lib/main.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+require 'socket'
+#TODO: Configurability
+$irc_connection = TCPSocket.open('irc.cluenet.org', 6667) #CONFIG: Connection details
+def rawsend (line)
+  puts "--> #{line}"
+  $irc_connection.puts(line)
+end
+rawsend('NICK Rubot') #CONFIG: Nick
+rawsend('USER Rubot * * :Rubot') #CONFIG: Username, realname
+while ($line = $irc_connection.gets) do
+  puts "<-- #{$line}"
+  rawsend("PONG #{$line.split[1]}") if $line.split[1] == 'PING'
+end
