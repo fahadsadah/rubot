fccabcee1a868221a7ceb2074174ed52f7aa4ec9
Henrik Hodne <dvyjones@binaryhex.com>
2010-04-09 13:20:51 +0200

    Prettied the events lib

--- lib/events.rb
+++ lib/events.rb
@@ -1,18 +1,23 @@
 class Hook
-  @@hooks = Array.new
   def self.hooks
-    @@hooks
+    @hooks ||= []
   end
+
   def initialize(event, &callback)
-    #Add the hook to the list of hooks
-    @@hooks << {:event => event, :callback => callback}
+    # Add the hook to the list of hooks
+    Hook.hooks << { :event => event, :callback => callback }
   end
-end
-#This is only a shortcut
-def on(event, callback)
-  return Hook.new(event, callback)
 end
 
-def fireevent(event, parameters)
-  Hook.hooks.select { |hook| hook[:event] == event }.each { |hook| hook[:callback].call(parameters) }
+# This is only a shortcut
+def on(*args)
+  return Hook.new(*args)
+end
+
+def fireevent(event, arguments)
+  Hook.hooks.each { |hook|
+    if hook[:event] == event
+      hook[:callback].call(arguments)
+    end
+  }
 end
