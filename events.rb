5be5e9383bc6f718480575002298e5a833d5c49d
Fahad Sadah <fahadsadah@gmail.com>
2010-09-15 16:21:14 +0100

    Formatting only (spaces to tabs)

--- events.rb
+++ events.rb
@@ -1,23 +1,23 @@
 class Hook
-  def self.hooks
-    @hooks ||= []
-  end
+	def self.hooks
+		@hooks ||= []
+	end
 
-  def initialize(event, &callback)
-    # Add the hook to the list of hooks
-    Hook.hooks << { :event => event, :callback => callback }
-  end
+	def initialize(event, &callback)
+		# Add the hook to the list of hooks
+		Hook.hooks << { :event => event, :callback => callback }
+	end
 end
 
 # This is only a shortcut
 def on(event, &callback)
-  return Hook.new(event, &callback)
+	return Hook.new(event, &callback)
 end
 
 def fireevent(event, arguments)
-  Hook.hooks.each { |hook|
-    if hook[:event] == event
-      hook[:callback].call(arguments)
-    end
-  }
+	Hook.hooks.each { |hook|
+		if hook[:event] == event
+			hook[:callback].call(arguments)
+		end
+	}
 end
