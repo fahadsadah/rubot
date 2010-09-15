64bf1aaf4cb516d9368faec07386fa151fd641a3
Fahad Sadah <fahadsadah@gmail.com>
2010-09-15 17:50:52 +0100

    Added support for conditional hooks. This is crucial to the upcoming :command hook

--- events.rb
+++ events.rb
@@ -3,21 +3,30 @@ class Hook
 		@hooks ||= []
 	end
 
-	def initialize(event, &callback)
+	def initialize(event, conditions=false, &callback)
 		# Add the hook to the list of hooks
-		Hook.hooks << { :event => event, :callback => callback }
+		Hook.hooks << { :event => event, :conditions => conditions, :callback => callback }
 	end
 end
 
 # This is only a shortcut
-def on(event, &callback)
+def on(event, conditions=false, &callback)
 	return Hook.new(event, &callback)
 end
 
 def fireevent(event, arguments)
 	Hook.hooks.each { |hook|
 		if hook[:event] == event
-			hook[:callback].call(arguments)
+			if !hook[:conditions]
+				hook[:callback].call(arguments)
+			else
+				fire = true
+				hook[:conditions].each do
+					|k,v|
+					fire = false if arguments[k] != v
+				end
+				hook[:callback].call(arguments) if fire
+			end
 		end
 	}
 end
