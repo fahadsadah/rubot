50aa8878f7ae42cfa37a400caade327a8a15f203
Fahad Sadah <fahadsadah@gmail.com>
2010-09-15 16:38:23 +0100

    Added example configuration file. Made invite optional, and more useful.

--- scripts/invite.rb
+++ scripts/invite.rb
@@ -1,1 +1,10 @@
-on(:invite) { |e| rawsend("JOIN #{e[:channel]}") }
+on(:invite) do
+	|e|
+	if $config['invite']['join']
+		rawsend("JOIN #{e[:channel]}")
+		if $config['invite']['announce']
+			message = $config['invite']['message'].sub('$nick',e[:nick]).sub('$chan',e[:channel])
+			rawsend("PRIVMSG #{e[:channel]} :#{message}")
+		end
+	end
+end
