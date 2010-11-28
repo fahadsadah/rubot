9ca4e523e8256bd14c39bf0592bdbca625e69260
Fahad Sadah <fahadsadah@gmail.com>
2010-11-28 15:39:20 +0000

    Solved bug caused by empty lines.

--- command.rb
+++ command.rb
@@ -1,6 +1,6 @@
 on(:msg) do
 	|e|
-	if e[:message].split[0][0].chr == $config['command']['trigger']
+	if e[:message].split.length > 0 && e[:message].split[0][0].chr == $config['command']['trigger']
 		arguments = e
 		arguments[:arguments] = arguments[:message].split(' ',2)[1]
 		arguments[:command] = arguments[:message].split(' ',2)[0][1..-1]
